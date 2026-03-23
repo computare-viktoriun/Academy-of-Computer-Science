c805261f4c6b44065066dc4e22105986b9265153
Flávio Leão <88902325+computare-viktoriun@users.noreply.github.com>
2026-03-23 20:34:20 -0300

    Refactor Conta class with constructors and methods

--- Conta.java
+++ Conta.java
@@ -5,66 +5,73 @@
 
 /**
  *
- * @author flavio.victor
+ * @author Aluno
  */
-
-/*
-Q1. Crie a classe Conta com os getters e setters necessários da sua classe e os métodos
-sca, deposita, calculaRendimento e getDadosConta. Adicione o modificador de
-visibilidade (private, se necessário) para cada atributo(titular, numero, agencia e
-saldo) e método da classe Conta. Crie uma classe TestaConta nela crie uma
-referência a um objeto do tipo Conta no método main e tente modificar ou ler um
-de seus atributos privados. O que acontece?
-*/
 public class Conta {
+
     String titular;
     private int numero;
     private String agencia;
     private double saldo;
-    
-    Data dataDeAbertura = new Data();
-    
-    public String getTitular(){
-        return titular;
+
+    public Conta(){
+        /*
+        Q7. Faça com que sua classe Conta possa receber, opcionalmente, o nome do titular da
+        Conta durante a criação do objeto. Utilize construtores para obter esse resultado.
+        Dica: utilize um construtor sem argumentos também, pensando no caso em que a
+        pessoa não queira passar o titular da Conta. Na classe Conta seria algo assim:
+        */
     }
     
-    public void setTitular(String titular){
+    public Conta(String titular){
         this.titular = titular;
     }
     
-    public int getNumero(){
+    Data dataDeAbertura = new Data();
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public int getNumero() {
         return numero;
     }
-    
-    public void setNumero(int numero){
+
+    public void setNumero(int numero) {
         this.numero = numero;
     }
-    
-    public String getAgencia(){
+
+    public String getAgencia() {
         return agencia;
     }
-    
-    public void setAgencia(String agencia){
+
+    public void setAgencia(String agencia) {
         this.agencia = agencia;
     }
-         
-    public double getSaldo(){
+
+    public double getSaldo() {
         return saldo;
     }
-    
-    public void setSaldo(double saldo){
+
+    public void setSaldo(double saldo) {
         this.saldo = saldo;
-    }            
-            
-    public void saca(double valor) {}
-    
-    public void deposita(double valor){
+    }
+
+    public void saca(double valor) {
+    }
+
+    public void deposita(double valor) {
         this.saldo += valor;
     }
-    
-    public double calculaRedimento(){
+
+    public double calculaRedimento() {
         return this.saldo * 0.1;
     }
+
     /*
         Q2. Na classe Conta, modifique o método getDadosConta() que não recebe parâmetro,
         mas devolve o texto com todas as informações da nossa conta para efetuarmos a
@@ -72,14 +79,16 @@ public class Conta {
         System.out.println() para cada mudança e teste que fizer com os seus funcionários,
         você simplesmente fará:
         
-    */
-    public String getDadosConta(){
+     */
+
+    public String getDadosConta() {
+        /*
+        Q6. Modifique seu método getDadosConta para que ele devolva o valor da
+        dataDeAbertura daquela Conta
+        */
         String dados = "Titular: " + this.titular;
-        dados += "\nNúmero: " + this.numero;
-        dados += "\nDia: " + this.dataDeAbertura.dia;
-        dados += "\nMês: " + this.dataDeAbertura.mes;
-        dados += "\nAno: " + this.dataDeAbertura.ano;
+        dados += "\nNumero: " + this.numero;
+        dados += "\nData de abertura: " + this.dataDeAbertura.formatada();
         return dados;
     }
-    
 }
