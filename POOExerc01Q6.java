9a6e005a150e0a9146a345dfdf66fa79ded0a566
Flávio Leão <88902325+computare-viktoriun@users.noreply.github.com>
2026-03-23 20:46:46 -0300

    Add parity check and processing in Exercicio06
    Implement logic to process an integer based on its parity.

---
+++ POOExerc01Q6.java
@@ -0,0 +1,17 @@
+public class Exercicio06 {
+   public static void main(String[] args){
+       int x = 1;
+       
+       if (x%2 ==0) {
+       
+           x = x / 2;
+           System.out.println(x);
+           
+       }
+       else {
+           x= 3 * x + 1;
+           System.out.println(x);
+       }
+   
+   }
+}
