221adacf5f5e2da5723a2f6ef90e42b1a236e0b7
Flávio Leão <88902325+computare-viktoriun@users.noreply.github.com>
2026-03-23 20:34:00 -0300

    Refactor Data class and add formatted date method

--- Data.java
+++ Data.java
@@ -5,17 +5,24 @@
 
 /**
  *
- * @author flavio.victor
+ * @author Aluno
  */
-class Data {
-    
+public class Data {
+
     int dia;
     int mes;
     int ano;
-    
+
     public void preencheData(int dia, int mes, int ano) {
         this.dia = dia;
         this.mes = mes;
         this.ano = ano;
-    } 
+    }
+    
+    public String formatada(){
+        /*
+        Q8.
+        */
+        return this.dia + "/" + this.mes + "/" + this.ano;
+    }
 }
