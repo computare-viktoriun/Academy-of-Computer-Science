46ee48c37776343ee11975529170961e0e550f16
Flávio Leão <88902325+computare-viktoriun@users.noreply.github.com>
2026-03-23 20:45:52 -0300

    Implement factorial calculation from 1 to 10
    Calculates and prints the factorials of numbers from 1 to 10.

---
+++ POOExerc01Q04.java
@@ -0,0 +1,28 @@
+public class Exercicio04 {
+    public static void main (String[] args) {
+    
+        /**
+        4. Imprima os fatoriais de 1 a 10. 
+        * O fatorial de um número n é n * (n-1)* (n-2)*...* 1.
+        * Lembre-se de utilizar os parênteses.
+        * O fatorial de 0 é 1
+        * O fatorial de 1 é (0!) * 1 = 1
+        * O fatorial de 2 é (1!) * 2 = 2
+        * O fatorial de 3 é (2!) * 3 = 6
+        * O fatorial de 4 é (3!) * 4 = 24
+        * Faça um for que inicie uma variável n (número) como 1, fatorial (resultado) como 1 e varia n de 1 até 10:
+        * int fatorial = 1;
+        * for (int n = 1; n <= 10; n++) {
+        * 
+        * }
+        */
+        
+        
+        int fatorial = 1;
+        for(int n = 1; n <= 10; n++) {
+            fatorial = (fatorial*n);
+            System.out.println(fatorial);
+        }
+        
+    }
+}
