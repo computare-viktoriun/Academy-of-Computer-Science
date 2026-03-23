d174c152d7e83c643332dafd3dec0eee12bc8e7c
Flávio Leão <88902325+computare-viktoriun@users.noreply.github.com>
2026-03-23 20:41:35 -0300

    Implement loop to print numbers from 150 to 300

---
+++ POOExerc01.java
@@ -0,0 +1,11 @@
+public class Exercicio01 {
+    public static void main(String[] args) {
+        /**
+        1. Imprima todos os números de 150 a 300. 
+        */
+        
+        for (int i = 150;i < 301; i++) {
+            System.out.println(i);
+        }
+    }
+}
