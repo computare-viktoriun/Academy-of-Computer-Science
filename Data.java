61b79ed5cebd07933c3b9989b931b68ce7dda9aa
Flávio Leão <88902325+computare-viktoriun@users.noreply.github.com>
2026-03-23 18:17:08 -0300

    Add Data class with date fields and method

---
+++ Data.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author flavio.victor
+ */
+class Data {
+    
+    int dia;
+    int mes;
+    int ano;
+    
+    public void preencheData(int dia, int mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    } 
+}
